dd0117f6a2ec134a75ef55b78b1dd81cfa8ef510
vovador <vovadorko@gmail.com>
2021-01-14 16:44:43 +0200

    Updates

---
+++ precache-manifest.ccfce41d14bfe6bf6ed8a7315f14dc1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89828ea1f8d72638786111bf541c10e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05a484e7e774db4d677d",
+    "url": "./static/css/main.0e6ed2ce.chunk.css"
+  },
+  {
+    "revision": "746b69c3a2dcf522ae55",
+    "url": "./static/js/2.a03797b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a03797b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a484e7e774db4d677d",
+    "url": "./static/js/main.68f69f07.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
